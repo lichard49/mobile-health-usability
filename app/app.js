e15350b148a9ac752d133fa57a8efe1bac84bc75
Richard Li <lichard49@gmail.com>
2022-07-04 00:48:26 -0700

    Demonstrate navigating between pages

---
+++ app/app.js
@@ -0,0 +1,8 @@
+// handle button to move to measurement page
+
+const toMeasurementPageButton =
+    document.getElementById('toMeasurementPageButton');
+
+toMeasurementPageButton.addEventListener('click', () => {
+  window.location = 'measurement.html';
+});
